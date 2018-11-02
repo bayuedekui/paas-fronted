da7f5c3815f9085d3e5a41e7a7968fa031bfd6e5
zhengkui <468360786@qq.com>
2018-11-02 19:30:46 +0800

    增加ajax的封装

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -4,6 +4,36 @@
  */
 import axios from 'axios'
 
-export default function ajax(params) {
-    
+export default function ajax(url, data = {}, type = 'GET') {
+
+  return new Promise(function (resolve, reject) {
+    //执行异步ajax请求
+    let promise
+    if (type == 'GET') {
+      //准备query请求参数
+      let dataStr = '' //数字拼接字符串
+      Object.keys(data).forEach(key => {
+        dataStr += key + '=' + data[key] + '&'
+      })
+      if (dataStr != null) {
+        dataStr = dataStr.lastIndexOf('&')
+        url = url + '?' + dataStr
+      }
+      //像后台发送ajax get请求
+      promise = axios.get(url)
+    } else {
+      //向后台发送post请求
+      promise = axios.post(url, data)
+    }
+
+    //成功了返回promise中的data数据，失败了返回error信息
+    promise.then(function (response) {
+      //成功了调用resolve()
+      resolve(response.data)
+    }).catch(function (error) {
+      //失败了调用reject()
+      reject(error)
+    })
+  })
+
 }
